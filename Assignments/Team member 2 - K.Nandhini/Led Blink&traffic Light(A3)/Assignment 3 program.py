ce308e92de877aad2c4899235506eca33eeb9e99
NandhiniK077 <114046810+NandhiniK077@users.noreply.github.com>
2022-10-19 11:35:09 +0530

    Assignment 3 works

---
+++ Assignments/Team member 2 - K.Nandhini/Led Blink&traffic Light(A3)/Assignment 3 program.py
@@ -0,0 +1,24 @@
+import time
+
+while True:
+
+
+    print("Red")
+    print("Blink!!")
+    time.sleep(1)
+    print("Blink!!")
+
+
+    print("Yellow")
+    print("Blink!!")
+    time.sleep(2)
+    print("Blink!!")
+
+
+    print("Green")
+    print("Blink!!")
+    time.sleep(1)
+    print("Blink!!")
+
+
+wn.mainloop()
